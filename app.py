01acb783c3e5e01db549bb3fc1042dc24d797e18
Eric Martin <eric@Erics-MacBook-Pro.local>
2025-11-08 10:02:35 -0700

    added db_test route

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, World from Eric Martin in 3308"
+
+
+@app.route('/db_test')
+def db_test():
+    try:
+        conn = psycopg2.connect(
+            "postgresql://emthedm_lab10db_user:YfR3U6na5R67hlYC4Vdmf40iYGbCHrAC@dpg-d47mupi4d50c7389sdc0-a/emthedm_lab10db")
+        conn.close()
+        return "Database connection successful!"
+    except Exception as e:
+        return f"Database connection failed: {e}"
