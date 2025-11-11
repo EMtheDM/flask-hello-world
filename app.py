d00453d6aa4405dc2232842d22fb5878234a886e
Eric Martin <eric@Erics-MacBook-Pro.local>
2025-11-10 17:51:25 -0700

    added db_insert route

--- app.py
+++ app.py
@@ -19,10 +19,12 @@ def db_test():
     except Exception as e:
         return f"Database connection failed: {e}"
 
+
 @app.route('/db_create')
 def db_create():
     try:
-        conn = psycopg2.connect("postgresql://emthedm_lab10db_user:YfR3U6na5R67hlYC4Vdmf40iYGbCHrAC@dpg-d47mupi4d50c7389sdc0-a/emthedm_lab10db")
+        conn = psycopg2.connect(
+            "postgresql://emthedm_lab10db_user:YfR3U6na5R67hlYC4Vdmf40iYGbCHrAC@dpg-d47mupi4d50c7389sdc0-a/emthedm_lab10db")
         cur = conn.cursor()
 
         cur.execute('''
@@ -39,3 +41,27 @@ def db_create():
         return "Basketball table created successfully!"
     except Exception as e:
         return f"Error creating table: {e}"
+
+
+@app.route('/db_insert')
+def db_insert():
+    try:
+        conn = psycopg2.connect(
+            "postgresql://emthedm_lab10db_user:YfR3U6na5R67hlYC4Vdmf40iYGbCHrAC@dpg-d47mupi4d50c7389sdc0-a/emthedm_lab10db")
+        cur = conn.cursor()
+
+        cur.execute('''
+            INSERT INTO Basketball (First, Last, City, Name, Number)
+            VALUES
+            ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+            ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+            ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+            ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2),
+            ('Eric', 'Martin', 'CU Boulder', 'Buffs', 3308);
+        ''')
+
+        conn.commit()
+        conn.close()
+        return "Basketball table populated successfully!"
+    except Exception as e:
+        return f"Error inserting data: {e}"
