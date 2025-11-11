07e0b86bb4b22bc59f5ea4fbb79612fc5648917b
Eric Martin <eric@Erics-MacBook-Pro.local>
2025-11-10 17:42:01 -0700

    added db_create route

--- app.py
+++ app.py
@@ -18,3 +18,24 @@ def db_test():
         return "Database connection successful!"
     except Exception as e:
         return f"Database connection failed: {e}"
+
+@app.route('/db_create')
+def db_create():
+    try:
+        conn = psycopg2.connect("postgresql://emthedm_lab10db_user:YfR3U6na5R67hlYC4Vdmf40iYGbCHrAC@dpg-d47mupi4d50c7389sdc0-a/emthedm_lab10db")
+        cur = conn.cursor()
+
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS test_table (
+                First VARCHAR(255),
+                Last VARCHAR(255),
+                City VARCHAR(255),
+                Name VARCHAR(255),
+                Number INT
+                );
+        ''')
+        conn.commit()
+        conn.close()
+        return "Basketball table created successfully!"
+    except Exception as e:
+        return f"Error creating table: {e}"
